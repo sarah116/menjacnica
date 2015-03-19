69d411f3644978cf9aca5518daba6ae84aab322f
sarah116 <saraahadzic@gmail.com>
2015-03-19 17:47:09 +0100

    Napravljena klasa Valuta sa privatnim atributima

---
+++ Menjacnica/src/menjacnica/valute/Valuta.java
@@ -0,0 +1,44 @@
+package menjacnica.valute;
+
+public class Valuta {
+
+	private String imeValute;
+	private String skraceniNaziv;
+	private int prodajniKurs;
+	private int kupovniKurs;
+	private int srednjiKurs;
+	public String getImeValute() {
+		return imeValute;
+	}
+	public void setImeValute(String imeValute) {
+		this.imeValute = imeValute;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public int getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(int prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public int getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(int kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public int getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(int srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	
+
+
+
+}
