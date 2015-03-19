0066bbd072f99b4a1591002b6d32d7a2bf189ced
sarah116 <saraahadzic@gmail.com>
2015-03-19 18:03:59 +0100

    Dodata logicka kontrola kod svih set metoda privatnih atributa klase Valuta

--- Menjacnica/src/menjacnica/valute/Valuta.java
+++ Menjacnica/src/menjacnica/valute/Valuta.java
@@ -7,36 +7,57 @@ public class Valuta {
 	private int prodajniKurs;
 	private int kupovniKurs;
 	private int srednjiKurs;
+
 	public String getImeValute() {
 		return imeValute;
 	}
+
 	public void setImeValute(String imeValute) {
+		if (imeValute == null)
+			throw new RuntimeException("Ime valute ne sme biti null");
 		this.imeValute = imeValute;
 	}
+
 	public String getSkraceniNaziv() {
 		return skraceniNaziv;
 	}
+
 	public void setSkraceniNaziv(String skraceniNaziv) {
+		if (skraceniNaziv == null)
+			throw new RuntimeException("Skraceno ime valute ne sme biti null");
 		this.skraceniNaziv = skraceniNaziv;
 	}
+
 	public int getProdajniKurs() {
 		return prodajniKurs;
 	}
+
 	public void setProdajniKurs(int prodajniKurs) {
+		if (prodajniKurs == 0)
+			throw new RuntimeException("Kurs ne sme biti 0");
 		this.prodajniKurs = prodajniKurs;
 	}
+
 	public int getKupovniKurs() {
 		return kupovniKurs;
 	}
+
 	public void setKupovniKurs(int kupovniKurs) {
+		if (kupovniKurs == 0)
+			throw new RuntimeException("Kurs ne sme biti 0");
 		this.kupovniKurs = kupovniKurs;
 	}
+
 	public int getSrednjiKurs() {
 		return srednjiKurs;
 	}
+
 	public void setSrednjiKurs(int srednjiKurs) {
+		if (srednjiKurs == 0)
+			throw new RuntimeException("Kurs ne sme biti 0");
 		this.srednjiKurs = srednjiKurs;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -50,6 +71,7 @@ public class Valuta {
 		result = prime * result + srednjiKurs;
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -77,6 +99,7 @@ public class Valuta {
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
 		return "Valuta [imeValute=" + imeValute + ", skraceniNaziv="
@@ -84,8 +107,5 @@ public class Valuta {
 				+ ", kupovniKurs=" + kupovniKurs + ", srednjiKurs="
 				+ srednjiKurs + "]";
 	}
-	
-
-
 
 }
