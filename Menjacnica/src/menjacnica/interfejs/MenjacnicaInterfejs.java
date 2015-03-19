11ba5017e81c26913c19cba8b38a5d3672b72c73
sarah116 <saraahadzic@gmail.com>
2015-03-19 17:58:47 +0100

    Dodat interfejs Menjacnica sa metodama dodajKurs, obrisiKurs i nadjiIVratiKurs.

---
+++ Menjacnica/src/menjacnica/interfejs/MenjacnicaInterfejs.java
@@ -0,0 +1,9 @@
+package menjacnica.interfejs;
+
+public interface MenjacnicaInterfejs {
+	
+	public void dodajKurs(String datum);
+	public void obrisiKurs(String datum);
+	public int nadjiIVratiKurs();
+
+}
